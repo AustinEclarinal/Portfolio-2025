d17a60e2ddf4e3f1eb0c154ab8d188fac59b9d93
Austin Paul P. Eclarinal <austinpauleclarinal@gmail.com>
2026-01-12 23:50:06 -0800

    Getting back on the project

--- src/data/experienceData.ts
+++ src/data/experienceData.ts
@@ -10,31 +10,39 @@ export interface Experience {
 
 export const experiences: Experience[] = [
   {
+    id: 5,
+    title: "CCS Day 2026",
+    role: "Business Manager",
+    date: "January 2026",
+    image: "https://placehold.co/600x800/2f9e44/FFF?text=CCS+Days+2026",
+  },
+  {
     id: 1,
     title: "CCS General Assembly",
-    role: "Business Manager",
+    role: "Chairperson",
     date: "Aug 2025",
     image: "https://placehold.co/600x800/25262b/FFF?text=General+Assembly", 
   },
   {
     id: 2,
     title: "JPCS Induction",
-    role: "President",
+    role: "JPCS President",
     date: "Mar 2025",
     image: "https://placehold.co/600x800/1971c2/FFF?text=President",
   },
   {
     id: 3,
     title: "IQ League: Vanguard",
-    role: "Chairperson",
+    role: "JPCS President",
     date: "June 2025",
     image: "https://placehold.co/600x800/e8590c/FFF?text=IQ+League",
   },
   {
     id: 4,
-    title: "Community Outreach",
+    title: "Tree of Hope 2024: Deck the Dreams",
     role: "Organizer",
     date: "Dec 2024",
     image: "https://placehold.co/600x800/2f9e44/FFF?text=Outreach",
   },
+  
 ];
